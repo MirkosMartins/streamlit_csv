7dfd055cbbc926fae6136dbb01b4140bda916efc
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-06-08 10:37:15 -0300

    Lendo csv
    retirado o header

--- tabela.py
+++ tabela.py
@@ -8,7 +8,7 @@ Created on Wed Jun  8 08:18:56 2022
 import streamlit as st
 import pandas as pd
 
-dataset = pd.read_csv('./nomes.csv',sep=';',header=0)
+dataset = pd.read_csv('./nomes.csv')
 st.write('Teste do uso de csv como armazenamento online.')
 st.table(dataset)
 st.text_input("Digite um novo nome para inserir:",key="novoNome")
