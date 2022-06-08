d9a2ac2d76605ffde67e2509cb747e513685fb9c
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-06-08 11:04:56 -0300

    Tabela 25

--- tabela.py
+++ tabela.py
@@ -8,7 +8,7 @@ Created on Wed Jun  8 08:18:56 2022
 import streamlit as st
 import pandas as pd
 
-dataset = pd.read_excel('nomes.xlsx',header=0,names=['Nome'])
+dataset = pd.read_excel('./nomes.xlsx',header=0,names=['Nome'])
 st.write('Teste do uso de csv como armazenamento online.')
 st.table(dataset)
 st.text_input("Digite um novo nome para inserir:",key="novoNome")
@@ -16,5 +16,5 @@ novo = pd.Series([str(st.session_state.novoNome)])
 n = pd.DataFrame(novo,columns=['Nome'])
 pd.concat([dataset,n])
 if st.button('Salvar'):
-    dataset.to_excel('nomes.xlsx',index=False)
+    dataset.to_excel('./nomes.xlsx',index=False)
 
